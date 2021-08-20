fc20d2abc9164c253a472cf0b179a2efc99fc89b
Alexploy <preon@bk.ru>
2021-08-20 12:44:30 +0300

    вывел спарсеную инфу в поля формы (частично)
    Signed-off-by: Alexploy <preon@bk.ru>

--- app/main.py
+++ app/main.py
@@ -34,9 +34,20 @@ class Ofd(QtWidgets.QMainWindow):
     def search_inn(self):
         input_search_str = self.ui.inn.text()
         print(input_search_str, type(input_search_str))
-        client = Parser(input_search_str)
-        ofd_data = client.get_ofd_data()
-        print(ofd_data)
+        client = Parser(input_search_str)  # создали объект класса Parser
+        ofd_data = client.get_ofd_data() # Вызвали метод get_ofd_data, получили данные для офд
+        # fio = ofd_data['фио']
+        # print(fio)
+        self.ui.label_top_info.setText(str(f"{ofd_data['info']} code: {ofd_data['status_code']} "))
+        self.ui.fio_dir.setText(str(ofd_data['фио']))
+        self.ui.fns.setText(str(ofd_data['фнс']))
+        self.ui.pfr.setText(str(ofd_data['пфр']))
+        self.ui.rosstat.setText(str(ofd_data['росстат']))
+        self.ui.fss.setText(str(ofd_data['фсс']))
+
+        # print(ofd_data)
+
+
 
 def main(): # точка входа
     app = QtWidgets.QApplication([])
